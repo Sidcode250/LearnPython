12fd54c4124a09749811674eccd72463b82b49d9
Sidcode250 <sidcode25@gmail.com>
2024-06-19 20:00:48 +0530

    Commit1

--- LearnPy1.py
+++ LearnPy1.py
@@ -1,7 +1,22 @@
 #print("Learning Python")
 
-mylist = ["zero", "one", "two", "three"]
+#mylist = ["zero", "one", "two", "three"]
 #print(mylist[0:3])
 
-mylist[1] = "ONE"
-print(mylist)
+#mylist[1] = "ONE" #free line 3
+#print(mylist)
+
+'''
+l = [1,2,3,4]
+m = [5,6,7,8]
+n = l + m
+print(n[1:6:2])
+n.pop()
+print(n)
+n.pop(2)
+print(n)
+print(n.pop(4))
+print(n)
+'''
+
+print("Sid")
