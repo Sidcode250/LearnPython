11ccdb572915b0900a3a98f8a6729c7def6e7713
Sidcode250 <sidcode25@gmail.com>
2024-06-22 03:41:49 +0530

    func2

--- LearnPy2.py
+++ LearnPy2.py
@@ -18,9 +18,31 @@ def say_hello(name):
 say_hello("Siddharth")
 '''
 
+'''
 def even(num):
     if num % 2 == 0:
         print("The number is even")
     else:
         print("The number is odd")
 even(26)
+''' 
+
+'''
+def fun2(list):
+    for num in list:
+        if num % 2 == 0:
+            print(num,"= Even")
+        else:
+            print(num,"= Odd")
+fun2([2,4,5,7,9,10])
+'''
+
+def even_list(list):
+    empty_list = []
+    for num in list:
+        if num % 2 == 0:
+            empty_list.append(num)
+        else:
+            pass
+    print(empty_list)
+even_list([2,5,7,10])
