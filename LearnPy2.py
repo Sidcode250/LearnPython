ea2b315b8833bb3439e0a243d8ccc363a953b054
Sidcode250 <sidcode25@gmail.com>
2024-06-22 03:09:58 +0530

    Func1

--- LearnPy2.py
+++ LearnPy2.py
@@ -6,6 +6,21 @@ def func_name(name):
 func_name('Sid')
 '''
 
+'''
 def fun_add(a,b):
     print(a+b)
 fun_add(2,3)
+'''
+
+'''
+def say_hello(name):
+    print(f"Hello {name}")
+say_hello("Siddharth")
+'''
+
+def even(num):
+    if num % 2 == 0:
+        print("The number is even")
+    else:
+        print("The number is odd")
+even(26)
