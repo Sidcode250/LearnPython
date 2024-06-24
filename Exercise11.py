45a8fefa878cd3b316a52b4217490ed373a926a5
Sidcode250 <sidcode25@gmail.com>
2024-06-25 02:19:26 +0530

    commit

--- Exercise11.py
+++ Exercise11.py
@@ -5,11 +5,20 @@ def master_yoda(text):
     return " ".join(text.split()[::-1])
 master_yoda("I am yoda")
 '''
+'''
 def almost_there(n):
-    if n >= 100 or n >= 200:
+    if n >= 90 and n <= 110:
         return True
-    elif n <= 100 or n <= 100:
+    elif n >= 190 and n <= 210:
         return True
     else:
         return False
-almost_there(104)
+'''
+
+def paper_doll(text):
+    mylist = []
+    for i in text:
+        mylist.append(i*3)
+    print("".join(mylist))
+    return "".join(mylist)
+paper_doll("hello")
