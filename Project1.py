d3c51ed29c06b896cef0a89be661e5beea5da3e7
Sidcode250 <sidcode25@gmail.com>
2024-06-29 05:45:48 +0530

    game

--- Project1.py
+++ Project1.py
@@ -14,4 +14,29 @@ def position_choice():
 
     return int(choice)
 
-position_choice()
+def replacement_choice(game_list,position):
+    user_placement = input("entee a string to to enter")
+    game_list[position] = user_placement
+    return game_list
+
+def gameon_choise():
+    choice = 'wrong'
+
+    while choice not in ['Y','N']:
+        choice = input("pick Y or N")
+        if choice not in ['Y' or 'N']:
+            print("please choose Y or N")
+    
+    if choice == 'Y':
+        return True
+    else:
+        return False
+    
+game_on = True
+
+while game_on == True:
+    display_game(game_list)
+    position = position_choice()
+    game_list = replacement_choice(game_list,position)
+    display_game(game_list)
+    game_on = gameon_choise() 
