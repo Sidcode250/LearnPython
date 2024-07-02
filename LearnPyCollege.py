018db97165d63b85cb57f04dacd171797022006d
Sidcode250 <sidcode25@gmail.com>
2024-07-02 12:14:58 +0530

    commit2

--- LearnPyCollege.py
+++ LearnPyCollege.py
@@ -1,3 +1,4 @@
+'''
 a = [1,2,3,4]
 
 def multiply(list1):
@@ -8,3 +9,35 @@ def multiply(list1):
     return multi
 
 multiply(a)
+'''
+'''
+l1 = [1,2,3,4,5,6,7]
+even = l1[2:7:2]
+odd = l1[1:7:2]
+print(even)
+print(odd)
+'''
+'''
+l1 = [1,2,3,4,5,6,7]
+
+def func(list1,b):
+    even = []
+    odd = []
+    b = list1.index(int)
+    for int in list1:
+        if list1.index(int) % 2 == 0:
+            even = even.append(int)
+        else:
+            odd = odd.append(int)
+'''
+l1 = [1,2,-3,4,-5,6]
+positive = []
+negative = []
+for a in l1:
+    if a>0:
+        positive.append(a)
+    elif a == 0:
+        pass
+    else:
+        negative.append(a)
+print(positive,negative) 
